a95f2da5b518b4be74b239b7c8707bcbe0dab9b7
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-04-28 11:44:15 -0500

    ReGex

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/*
+var name = document.getElementById('input-name');
+var lastname = document.getElementById('input-lastname');
+var numberDNI = document.getElementById('input-ID-number');
+
+var letras = /([A-Z])\w+/;
+var letra = /[a-zA-Z]/;
+if(letra.test(name)){
+      alert("Estructura de nombre no válida");
+    }
+*/
